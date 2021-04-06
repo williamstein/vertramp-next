85886d118dc0bf484b1668601e4b8e120c6e1ed6
William Stein <wstein@gmail.com>
2021-04-06 21:45:34 +0000

    update readme

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-let basePath;
-if (process.env.BASE_PATH) {
-  basePath = process.env.BASE_PATH;
-} else if (process.env.NEXT_STATIC) {
-  basePath = `/${process.env.COCALC_PROJECT_ID}/raw/vertramp/out`;
-} else {
-  basePath = `/${process.env.COCALC_PROJECT_ID}/port/3000`;
+let basePath = undefined;
+if (process.env.COCALC_PROJECT_ID) {
+  if (process.env.BASE_PATH) {
+    basePath = process.env.BASE_PATH;
+  } else if (process.env.NEXT_STATIC) {
+    basePath = `/${process.env.COCALC_PROJECT_ID}/raw/vertramp/out`;
+  } else {
+    basePath = `/${process.env.COCALC_PROJECT_ID}/port/3000`;
+  }
 }
 
 module.exports = {
